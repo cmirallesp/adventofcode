268f32de61217b4e65aa4a522baaa8cc8bee66a9
Carles <cmirallesp@gmail.com>
2022-12-02 17:36:13 +0100

    Remove 01_calories folder

--- 01_calories/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to calories
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- 01_calories/test/calories/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns calories.core-test
-  (:require [clojure.test :refer :all]
-            [calories.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
